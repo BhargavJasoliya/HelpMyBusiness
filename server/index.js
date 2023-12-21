829c7dc182b901d3dfff66656af23adb69446bd0
BhargavJasoliya <bhargavjasoliya10@gmail.com>
2023-12-21 18:40:13 +0530

    compeleted product component

--- server/index.js
+++ server/index.js
@@ -11,6 +11,25 @@ import generalRoutes from './routes/general.js';
 import managementRoutes from './routes/management.js';
 import salesRoutes from './routes/sales.js';
 
+
+/** imports for data purpose only */
+// import User from './models/User.js';
+// import { dataUser } from './data/index.js';
+// import Product from "./models/Product.js";
+// import ProductStat from "./models/ProductStat.js";
+// import Transaction from "./models/Transaction.js";
+// import OverallStat from "./models/OverallStat.js";
+// import AffiliateStat from "./models/AffiliateStat.js";
+
+// import {
+//     dataUser,
+//     dataProduct,
+//     dataProductStat,
+//     dataTransaction,
+//     dataOverallStat,
+//     dataAffiliateStat,
+// } from "./data/index.js";
+
 // configurations
 dotenv.config();
 
@@ -35,4 +54,14 @@ mongoose
     .connect(process.env.MONGO_URL)
     .then(() => {
         app.listen(PORT, () => console.log(`Server connected on port: ${PORT}`));
+
+        /** DATA to be uploaded to the database only once */
+
+        // User.insertMany(dataUser);
+        // AffiliateStat.insertMany(dataAffiliateStat);
+        // OverallStat.insertMany(dataOverallStat);
+        // Product.insertMany(dataProduct);
+        // ProductStat.insertMany(dataProductStat);
+        // Transaction.insertMany(dataTransaction);
+
     }).catch((error) => console.log(`${error} did not connacted`));
